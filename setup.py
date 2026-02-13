35833b15ddb8153311ea38182adffcde7288ba12
devautomation <deepak_kr92@icloud.com>
2026-02-13 17:57:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Deepak Kumar",
+    author_email="deepak_kr92@icloud.com",
+    packages=find_packages(),
+    install_requires=[]
+)
